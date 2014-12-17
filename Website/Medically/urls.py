9e059ca3c783c9c6b7c9b9da88f0826289985881
Hakan Uyumaz <hakanuyumaz@gmail.com>
2014-12-17 13:27:55 +0200

    Index page added

--- Website/Medically/urls.py
+++ Website/Medically/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from MedicallyWeb import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Medically.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.homepage, name='homepage'),
 )

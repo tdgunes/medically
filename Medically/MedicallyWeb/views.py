681b4c4f21cc9f58dd187b1e3ff95a02b70bebde
Taha Doğan Güneş <tdgunes@gmail.com>
2014-12-19 04:22:48 +0200

    Update views.py, Add login feature

--- Medically/MedicallyWeb/views.py
+++ Medically/MedicallyWeb/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+
 def homepage(request):
-    return render(request, 'index.html', {"error": False, "error_message": ""})
+    if not request.user.is_authenticated():
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    # Redirect to a success page
+                    return redirect("Medically:homepage")
+                else:
+                    # Return a 'disabled account' error message
+                    return render(request, 'index.html',
+                                  {"error": True, "error_message": "Your account is disabled!"})
+
+            else:
+                # Return an 'invalid login' error message.
+                return render(request, 'index.html',
+                              {"error": True, "error_message": "Wrong credentials, try again!"})
+        else:
+            return render(request, 'index.html', {"error": False, "error_message": ""})
+    else:
+        return render(request, 'index.html', {"error": True, "error_message": "Success."})
+

f355d105d557e03faab835b33f299ff072775d48
Hakan Uyumaz <hakanuyumaz@gmail.com>
2014-12-27 20:21:04 +0200

    Fix examination model

--- web/models/examination.py
+++ web/models/examination.py
@@ -7,14 +7,14 @@ from . import Patient
 class Examination(models.Model):
 
     patient = models.ForeignKey(Patient)
-    title = models.CharField("Title", max_length=20, null=False)
+    title = models.CharField("Title", max_length=20, null=False, default="blank")
     date = models.DateField("Examination Date", null=False)
-    information = models.CharField("Examination Information", max_length=1000)
-    diagnosis = models.CharField("Diagnosis", max_length=1000)
-    offered_treatment = models.CharField("Offered Treatment", max_length=20)
+    information = models.CharField("Examination Information", max_length=1000, null=True)
+    diagnosis = models.CharField("Diagnosis", max_length=1000, null=True)
+    offered_treatment = models.CharField("Offered Treatment", max_length=20, null=True)
     surgery_type = models.CharField("Surgery Type", max_length=20, null=True)
     surgery_date = models.DateField("Surgery Date", null=True)
-    surgery_prep = models.CharField("Surgery Preparation", max_length=1000)
+    surgery_prep = models.CharField("Surgery Preparation", max_length=1000, null=True)
 
 
     def __unicode__(self):

ecb930e44640d48bc05bf3e4e2d45d53c73fdfab
Hakan Uyumaz <hakanuyumaz@gmail.com>
2014-12-29 22:14:23 +0200

    Email validation added

--- web/static/js/validation.js
+++ web/static/js/validation.js
@@ -6,15 +6,26 @@ function isValid(input){
 function disable(button) {
     button.attr("disabled", "disabled")
 }
+function enable(button) {
+    button.removeAttr("disabled");
+}
 function checkForm(mandatoryFields,button){
     for(var i=0;i<mandatoryFields.length;i++)
         if(!isValid(mandatoryFields[i])) {
             disable(button)
             return
         }
-    button.removeAttr("disabled");
+    enable(button)
 }
 
+function checkRegisterform(mandatorFields, button, checkbox, email) {
+    for (var i = 0; i < mandatoryFields.length; i++)
+        if (!isValid(mandatoryFields[i])) {
+            disable(button)
+            return
+        }
+    enable(button);
+}
 function validate(mandatoryFields, button){
     $(document).ready(function(){
         checkForm(mandatoryFields, button)
@@ -23,5 +34,23 @@ function validate(mandatoryFields, button){
                 checkForm(mandatoryFields,button)
             })
         }
+    })
+}
+function isEmail(emailInput) {
+    var email = emailInput.val()
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+function validateEmail(email, symbol) {
+    $(document).ready(function () {
+        email.blur(function () {
+            if (isEmail(email)) {
+                symbol.removeClass('glyphicon-remove')
+                symbol.addClass('glyphicon-ok')
+            } else {
+                symbol.removeClass('glyphicon-ok')
+                symbol.addClass('glyphicon-remove')
+            }
+        })
     })
 }

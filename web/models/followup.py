dcb90d74bb8f0c33aab96d3048087cae77d9b515
Hakan Uyumaz <hakanuyumaz@gmail.com>
2014-12-30 09:59:27 +0200

    Followup model updated

--- web/models/followup.py
+++ web/models/followup.py
@@ -1,3 +1,18 @@
 __author__ = 'tdgunes'
 
 from django.db import models
+from .examination import Examination
+
+
+class Followup(models.Model):
+    # MASI - MAndatory Surgery Information
+    examination = models.ForeignKey(Examination)
+    date = models.DateField(null=False, blank=False)
+
+
+    # OSI - Optional Surgery Information
+    information = models.TextField(max_length=1000, null=True, blank=True)
+    appointment_date = models.DateField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.full_name
